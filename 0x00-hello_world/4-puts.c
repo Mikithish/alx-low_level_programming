4a49036e47235ccd0c8c8eadeaa5524fa5377d46
Isheka <turayishekama93@gmail.com>
2022-04-07 08:34:07 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main prints out "Programming is like building a multilingual puzzle"
+ * Returns 0
+ */
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
