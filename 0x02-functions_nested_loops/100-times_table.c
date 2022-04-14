7fd69c6166950fb7b5d8570415a939b64609b375
Isheka <turayishekama93@gmail.com>
2022-04-14 03:32:44 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_times_tables - prints the times table
- * @n: integer for whick the times table will be printed
+ * print_times_table - prints the times table
+ * @n: integer for which the times table will be printed
  *
  * Description: prints the times table
  *
