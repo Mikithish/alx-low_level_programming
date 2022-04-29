27e473c039cbd31fd96a34d883555f417c0ae880
Isheka <turayishekama93@gmail.com>
2022-04-29 06:29:01 -0700

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#inclde "main.h"
+
+/**
+ * _pow_recursion - pow recursion
+ * @x: integer paramtr
+ * @y: integer paramtr
+ * Return: recursion
+ */
+
+int _pow_recursion(int x. int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y ++ 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
