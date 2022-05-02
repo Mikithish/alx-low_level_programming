56dd2e4afca08163cc7313b81d097fc040605ebf
Isheka <turayishekama93@gmail.com>
2022-05-02 10:17:16 -0700

    argc plus

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,15 +2,15 @@
 
 /**
  * main - prints all arguments it receives
- * @argc: n args
- * @argc: arr args
- * Return: 0
+ * @argc: number of command line arguments
+ * @argc: array that contains the program command line arguments
+ * Return: 0 - success
  */
+
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
-}
